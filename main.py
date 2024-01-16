f9961a2c8cadf387828a69e75245833bc069333c
Reuben James <20reuben.james@utcportsmouth.org>
2024-01-16 17:47:17 +0000

    Add move function to board

--- main.py
+++ main.py
@@ -10,8 +10,14 @@ class Board:
     def _display_horizontal_line(self):
         print("+---" * 3 + "+")
 
+    def _piece_lookup_reverse(self, character):
+        return list(self._piece_lookup.keys())[list(self._piece_lookup.values()).index(character)]
+
     def display(self):
         for i in self._grid:
             self._display_horizontal_line()
             print("¦ " + " ¦ ".join(self._piece_lookup[k] for k in i) + " ¦")
         self._display_horizontal_line()
+
+    def move(self, row, col, character):
+        self._grid[row][col] = self._piece_lookup_reverse(character)
